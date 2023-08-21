480d33a74031f45ad5a5b13a87db3eb8a6f4d701
CynthiaOuma12673 <oumacynthia817@gmail.com>
2023-08-21 12:55:00 +0300

    if statement to check on the pm/am conversions

--- clock.js
+++ clock.js
@@ -4,5 +4,16 @@ function myDigitalClock(){
     var minutes = date.getMinutes();
     var seconds =date.getSeconds();
     var zone = "AM";
-    
+
+
+// time conversion to 12hour system and am/pm determination
+    if (hours > 12) {
+        hours -=12;
+        zone ="PM";
+    }
+
+    if (hours === 0){
+        hours = 12;
+    }
+
 }
