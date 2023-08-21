ea66d4021e6e08784abc96096d150f977dbda14c
CynthiaOuma12673 <oumacynthia817@gmail.com>
2023-08-21 12:51:04 +0300

    function myDigitalClock declaration and variables declarations for getting date, hours, seconds, minutes and zone.

---
+++ clock.js
@@ -0,0 +1,8 @@
+function myDigitalClock(){
+    var date = new Date();
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds =date.getSeconds();
+    var zone = "AM";
+    
+}
